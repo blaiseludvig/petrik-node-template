34c726c4615bb31cdcec6ebcc5e903b653a0d227
blaiseludvig <blaiseludvig@users.noreply.github.com>
2022-10-04 23:05:15 +0000

    deploy: 13bdd82c5d6e0fc890fc3ae22203d9794af58233

---
+++ main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+// import { animateCSS } from "./animatecss";
+
+let element = document.createElement('h1');
+element.textContent = "Ha ezt látod, akkor működik!"
+document.body.append(element);
+
+/******/ })()
+;
